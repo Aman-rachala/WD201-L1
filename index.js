8f234ed46f9d5e58404f3f007c876b3bec44da8b
Aman Rachala <107355460+Aman-rachala@users.noreply.github.com>
2022-11-15 14:37:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+let hello = "Hello Node.js!"
+
+returnHello = () => {
+    console.log(hello);
+
+}
+
+returnHello();
+=======
+int hello - "Hello Node.js!"
+
+returnHello = () =>{
+   consol.log(hello);
+}
+returnHello();
+>>>>>>> 673a23461787025927ab674a11063998be34b123
